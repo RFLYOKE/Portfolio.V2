62e2d5dc29e67a578118be73701d3666cae30cfb
akmlrafly <akmalrafly875@gmail.com>
2026-02-10 20:08:09 +0700

    update: data experiences

--- src/constants/index.js
+++ src/constants/index.js
@@ -154,6 +154,18 @@ export const mySocials = [
 
 export const experiences = [
   {
+    title: "Full Time - Frontend Developer",
+    job: "PT Nadi Sukses Bersama",
+    date: "July 2025 - February 2026",
+    contents: [
+      "Developed and maintained modern web applications using Next.js and Tailwind CSS, ensuring high performance and SEO optimization.",
+      "Integrated RESTful APIs and managed global state to create seamless, dynamic, and interactive user experiences.",
+      "Collaborated with UI/UX designers to translate high-fidelity wireframes into responsive, pixel-perfect functional code.",
+      "Participated in code reviews and implemented best practices for clean, maintainable, and scalable codebase architecture.",
+      "Troubleshot and debugged issues to ensure cross-browser compatibility and mobile responsiveness.",
+    ],
+  },
+  {
     title: "Internship - Frontend Developer",
     job: "MySkin Web Application Development",
     date: "February 2025 - May 2025",
